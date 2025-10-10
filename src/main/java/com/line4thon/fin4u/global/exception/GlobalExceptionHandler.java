de887eb36a95b1272fc7eb5d2feb32ad3333c86e
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-10-11 00:49:11 +0900

    Feat: 예외 핸들링을 위한 객체 작성

---
+++ src/main/java/com/line4thon/fin4u/global/exception/GlobalExceptionHandler.java
@@ -0,0 +1,137 @@
+package com.line4thon.fin4u.global.exception;
+
+import com.line4thon.fin4u.global.response.ErrorResponse;
+import com.line4thon.fin4u.global.response.code.ErrorResponseCode;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindException;
+import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.multipart.support.MissingServletRequestPartException;
+import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    /**
+     * RequestBody DTO의 검증 조건을 위배한 경우
+     * @param e
+     */
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ErrorResponse<?>> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException e
+    ) {
+        log.error("MethodArgumentNotValidException : {}", e.getMessage(), e);
+        ErrorResponse<?> errorResponse = ErrorResponse.of(
+                ErrorResponseCode.INVALID_HTTP_MESSAGE_BODY, e.getFieldError().getDefaultMessage());
+        return ResponseEntity.status(errorResponse.getHttpStatus()).body(errorResponse);
+    }
+
+    /**
+     * RequestParam, ModelAttribute 객체의 검증 조건을 위배한 경우
+     * @param e
+     */
+    @ExceptionHandler(BindException.class)
+    public ResponseEntity<ErrorResponse<?>> handleBindException(BindException e) {
+        log.error("BindException : {}", e.getMessage(), e);
+        ErrorResponse<?> errorResponse = ErrorResponse.of(
+                ErrorResponseCode.INVALID_HTTP_MESSAGE_BODY, e.getFieldError().getDefaultMessage());
+        return ResponseEntity.status(errorResponse.getHttpStatus()).body(errorResponse);
+    }
+
+    /**
+     * JSON 문법 오류, 타입 불일치, 필수 바디 누락 등의 경우로 JSON 파싱에 실패한 경우
+     * @param e
+     */
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse<?>> handleHttpMessageNotReadableException(
+            HttpMessageNotReadableException e
+    ) {
+        log.error("HttpMessageNotReadableException : {}", e.getMessage(), e);
+        ErrorResponse<?> errorResponse = ErrorResponse.from(ErrorResponseCode.INVALID_HTTP_MESSAGE_BODY);
+        return ResponseEntity.status(errorResponse.getHttpStatus()).body(errorResponse);
+    }
+
+    /**
+     * enum, RequestParam 등의 요청 파라미터의 타입이 불일치한 경우
+     * @param e
+     */
+    @ExceptionHandler(MethodArgumentTypeMismatchException.class)
+    public ResponseEntity<ErrorResponse<?>> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+        log.error("MethodArgumentTypeMismatchException : {}", e.getMessage(), e);
+        ErrorResponse<?> errorResponse = ErrorResponse.from(ErrorResponseCode.INVALID_HTTP_MESSAGE_PARAMETER);
+        return ResponseEntity.status(errorResponse.getHttpStatus()).body(errorResponse);
+    }
+
+    /**
+     * multipart/form-data 에서 RequestPart가 누락된 경우
+     * @param e
+     */
+    @ExceptionHandler(MissingServletRequestPartException.class)
+    public ResponseEntity<ErrorResponse<?>> handleMissingServletRequestPartException(MissingServletRequestPartException e) {
+        log.error("MissingServletRequestPartException : {}", e.getMessage(), e);
+        ErrorResponse<?> errorResponse = ErrorResponse.from(ErrorResponseCode.INVALID_HTTP_MESSAGE_PARAMETER);
+        return ResponseEntity.status(errorResponse.getHttpStatus()).body(errorResponse);
+    }
+
+    /**
+     * 지원하지 않는 HTTP 메소드를 호출한 경우
+     * @param e
+     */
+    @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
+    public ResponseEntity<ErrorResponse<?>> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e) {
+        log.error("HttpRequestMethodNotSupportedException : {}", e.getMessage(), e);
+        ErrorResponse<?> errorResponse = ErrorResponse.from(ErrorResponseCode.UNSUPPORTED_HTTP_METHOD);
+        return ResponseEntity.status(errorResponse.getHttpStatus()).body(errorResponse);
+    }
+
+    /**
+     * 존재하지 않는 엔드포인트를 호출한 경우
+     * @param e
+     */
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ResponseEntity<ErrorResponse<?>> handleNoHandlerFoundException(NoHandlerFoundException e) {
+        log.error("NoHandlerFoundException : {}", e.getMessage(), e);
+        ErrorResponse<?> errorResponse = ErrorResponse.from(ErrorResponseCode.NOT_FOUND_ENDPOINT);
+        return ResponseEntity.status(errorResponse.getHttpStatus()).body(errorResponse);
+    }
+
+    /**
+     * (잘못된 path 등으로 인해) 정적 리소스 파일을 찾지 못한 경우
+     * @param e
+     */
+    @ExceptionHandler(NoResourceFoundException.class)
+    public ResponseEntity<ErrorResponse<?>> handleNoResourceFoundException(NoResourceFoundException e) {
+        log.error("NoResourceFoundException : {}", e.getMessage(), e);
+        ErrorResponse<?> errorResponse = ErrorResponse.from(ErrorResponseCode.NOT_FOUND_ENDPOINT);
+        return ResponseEntity.status(errorResponse.getHttpStatus()).body(errorResponse);
+    }
+
+    /**
+     * CustomException이 발생한 경우
+     * @param e
+     */
+    @ExceptionHandler(BaseException.class)
+    public ResponseEntity<ErrorResponse<?>> handleBaseException(BaseException e) {
+        log.error("BaseException : {}", e.getMessage(), e);
+        ErrorResponse<?> errorResponse = ErrorResponse.from(e.getBaseResponseCode());
+        return ResponseEntity.status(errorResponse.getHttpStatus()).body(errorResponse);
+    }
+
+    /**
+     * 발생할 경우를 상정하지 않은, 즉 알 수 없는 서버 예외를 여기서 핸들링
+     * @param e
+     */
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse<?>> handleException(Exception e) {
+        log.error("Exception : {}", e.getMessage(), e);
+        ErrorResponse<?> errorResponse = ErrorResponse.from(ErrorResponseCode.SERVER_ERROR);
+        return ResponseEntity.status(errorResponse.getHttpStatus()).body(errorResponse);
+    }
+}
