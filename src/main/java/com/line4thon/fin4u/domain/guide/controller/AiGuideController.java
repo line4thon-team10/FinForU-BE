a48f8e91dc170d9227c24a3b1e4e18f45b301166
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-02 01:31:36 +0900

    Feat: AI 가이드의 메인 페이지를 조회했을 때, 기본적으로 전달하는 데이터 API 작성

--- src/main/java/com/line4thon/fin4u/domain/guide/controller/AiGuideController.java
+++ src/main/java/com/line4thon/fin4u/domain/guide/controller/AiGuideController.java
@@ -1,5 +1,6 @@
 package com.line4thon.fin4u.domain.guide.controller;
 
+import com.line4thon.fin4u.domain.guide.dto.GetGuideMainPage;
 import com.line4thon.fin4u.global.response.SuccessResponse;
 import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/guide")
 @RequiredArgsConstructor
@@ -19,11 +23,37 @@ public class AiGuideController {
     private final ChatClient chatClient;
 
     @GetMapping
+    public ResponseEntity<SuccessResponse<?>> getMain(
+            // FIXME 사용자 언어 설정에 따른 분기점 작성
+    ) {
+        GetGuideMainPage response;
+        if(true) {
+            List<GetGuideMainPage.Faq> questions = new ArrayList<>();
+            questions.add(new GetGuideMainPage.Faq("How to open a bank account?"));
+            questions.add(new GetGuideMainPage.Faq("What are bank rules by country?"));
+            questions.add(new GetGuideMainPage.Faq("How do I stay safe from voice phishing or financial scams?"));
+
+            response = new GetGuideMainPage(
+                    "Hello. I'm your AI assistant.\n How can I help you today?",
+                    questions
+            );
+        }
+//        else if(user.getLanguage().equals("chinese")) {
+//
+//        } else if(user.getLanguage().equals("vietnamese")) {
+//
+//        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(SuccessResponse.ok(response));
+    }
+
+    @GetMapping("/query")
     public ResponseEntity<SuccessResponse<?>> getAiGuide(
             // FIXME JWT 추가 시, 토큰 검증 필요
             @RequestParam @NotBlank String message
     ) {
 
+        // FIXME 사용자의 언어 정보를 추가적으로 전달해주어 언어에 맞게 자연어처리 된 문장을 반환하도록 설정
         String response = chatClient.prompt(message).call().content();
 
         return ResponseEntity.status(HttpStatus.OK).body(SuccessResponse.empty());
