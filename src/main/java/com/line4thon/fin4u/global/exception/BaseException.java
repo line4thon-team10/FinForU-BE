548684066f6c8711374b2925e44514a013248834
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-01 23:52:43 +0900

    Feat: 예외에 대한 기본 구조 클래스 작성

---
+++ src/main/java/com/line4thon/fin4u/global/exception/BaseException.java
@@ -0,0 +1,11 @@
+package com.line4thon.fin4u.global.exception;
+
+import com.line4thon.fin4u.global.response.code.BaseResponseCode;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class BaseException extends RuntimeException {
+  private final BaseResponseCode baseResponseCode;
+}
