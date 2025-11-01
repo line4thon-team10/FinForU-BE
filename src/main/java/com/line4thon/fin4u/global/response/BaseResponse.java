736fb2a9e70b9055222891ab669b664a5e2a2b2a
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-01 23:40:54 +0900

    Feat: 응답을 정형화된 JSON으로 변환하는 기본 객체

---
+++ src/main/java/com/line4thon/fin4u/global/response/BaseResponse.java
@@ -0,0 +1,52 @@
+package com.line4thon.fin4u.global.response;
+
+import com.line4thon.fin4u.global.response.code.BaseResponseCode;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@Getter
+@RequiredArgsConstructor
+@ToString
+public class BaseResponse {
+
+    private final Boolean isSuccess;
+    private final String code;
+    private final String message;
+    private final String timeStamp = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+
+    public static BaseResponse of(Boolean isSuccess, BaseResponseCode baseResponseCode) {
+        return new BaseResponse(
+                isSuccess,
+                baseResponseCode.getCode(),
+                baseResponseCode.getMessage()
+        );
+    }
+
+    public static BaseResponse of(Boolean isSuccess, BaseResponseCode baseResponseCode, String message) {
+        return new BaseResponse(
+                isSuccess,
+                baseResponseCode.getCode(),
+                message
+        );
+    }
+
+    public static BaseResponse of(Boolean isSuccess, String code, String message) {
+        return new BaseResponse(
+                isSuccess,
+                code,
+                message
+        );
+    }
+
+    public static BaseResponse of(BaseResponseCode baseResponseCode) {
+        return new BaseResponse(
+                false,
+                baseResponseCode.getCode(),
+                baseResponseCode.getMessage()
+        );
+    }
+}
