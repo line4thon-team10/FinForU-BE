0955cc554133f23c7bfa819d86e5cc5b3172ca5c
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-03 21:28:48 +0900

    Feat: 환율 조회 서비스의 구체 클래스 선언

---
+++ src/main/java/com/line4thon/fin4u/domain/exrate/service/ExchangeRateServiceImpl.java
@@ -0,0 +1,18 @@
+package com.line4thon.fin4u.domain.exrate.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RequiredArgsConstructor
+public class ExchangeRateServiceImpl implements ExchangeRateService {
+    @Override
+    public Object getExchangeRateGraphData() {
+        return null;
+    }
+
+    @Override
+    public Object getToastMessage() {
+        return null;
+    }
+}
