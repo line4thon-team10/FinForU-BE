36085a02d8e6efeb66047e8af62b912e2604ffa2
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-04 22:33:37 +0900

    Fix: 나라별로 환전 데이터를 구축하여 배열로 반환하는 환전 Service 구현체의 getExchangeRateGraph 함수 구현

--- src/main/java/com/line4thon/fin4u/domain/exrate/service/ExchangeRateServiceImpl.java
+++ src/main/java/com/line4thon/fin4u/domain/exrate/service/ExchangeRateServiceImpl.java
@@ -1,14 +1,37 @@
 package com.line4thon.fin4u.domain.exrate.service;
 
+import com.line4thon.fin4u.domain.exrate.KoreanBank.Country;
+import com.line4thon.fin4u.domain.exrate.web.dto.ExchangeRateRes;
+import com.line4thon.fin4u.domain.exrate.KoreanBank.KoreanBankApiProvider;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Slf4j
+@Service
 @RequiredArgsConstructor
 public class ExchangeRateServiceImpl implements ExchangeRateService {
+
+    private final KoreanBankApiProvider provider;
+
     @Override
-    public Object getExchangeRateGraphData() {
-        return null;
+    public List<ExchangeRateRes> getExchangeRateGraphData() {
+        ExchangeRateRes usdExchangeRate = provider
+                .callExchangeRate(Country.USD_CODE.getCurrencyCode()
+                );
+
+        ExchangeRateRes cnyExchangeRate = provider
+                .callExchangeRate(
+                        Country.CNY_CODE.getCurrencyCode()
+                );
+
+        ExchangeRateRes vndExchangeRate = provider
+                .callExchangeRate(
+                        Country.VND_CODE.getCurrencyCode()
+                );
+        return List.of(usdExchangeRate, cnyExchangeRate, vndExchangeRate);
     }
 
     @Override
