820da67ca2c677768ec3eb79b1904235f5c70700
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-05 12:47:32 +0900

    Fix: 클라이언트에게 전달할 Res 객체에 각 화폐별로 은행사 최대 우대 환전 수수료를 전달할 수 있도록 필드 추가

--- src/main/java/com/line4thon/fin4u/domain/exrate/web/dto/ExchangeRateRes.java
+++ src/main/java/com/line4thon/fin4u/domain/exrate/web/dto/ExchangeRateRes.java
@@ -7,13 +7,15 @@ public record ExchangeRateRes(
         String currencyType,   // 외화 종류
         Double todayRate,                // 현재 가격
         Double rateCompareYesterday,  // 어제와 비교해서 n.n%
-        // String duration,                // 기간
-        List<Exchange> priceGraphData     // 환율 그래프를 위한 데이터(날짜와 가격)
+        List<Exchange> priceGraphData,     // 환율 그래프를 위한 데이터(날짜와 가격)
+        List<Fee> eachBankFee
 ) {
     public record Exchange(
             LocalDate date,
             Double price
-    ) {
-
-    }
+    ) {}
+    public record Fee(
+            String bank,
+            Double fee
+    ) {}
 }
