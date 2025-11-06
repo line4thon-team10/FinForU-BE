4f4da446012c8dcec5d843817432f7b133374f13
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-06 15:41:06 +0900

    Fix: 최상위 응답 객체의 필드로 건넬 AI 토스트 메세지 문자열 변수 추가

--- src/main/java/com/line4thon/fin4u/domain/exrate/web/dto/ExchangeRateRes.java
+++ src/main/java/com/line4thon/fin4u/domain/exrate/web/dto/ExchangeRateRes.java
@@ -4,12 +4,16 @@ import java.time.LocalDate;
 import java.util.List;
 
 public record ExchangeRateRes(
+        ExchangeRateData ExchangeRateData,
+        String toastMessage
+){
+    public record ExchangeRateData(
         String currencyType,   // 외화 종류
         Double todayRate,                // 현재 가격
         Double rateCompareYesterday,  // 어제와 비교해서 n.n%
         List<Exchange> priceGraphData,     // 환율 그래프를 위한 데이터(날짜와 가격)
         List<Fee> eachBankFee
-) {
+    ) {}
     public record Exchange(
             LocalDate date,
             Double price
