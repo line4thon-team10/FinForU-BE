7c73abb79485579c6100eef14cf5de34a7089b32
LgE02 <leegaeun023@gmail.com>
2025-11-06 13:07:50 +0900

    chore: 기존 스프링 작동 테스트의 db체크 제외

--- src/test/java/com/line4thon/fin4u/Fin4uApplicationTests.java
+++ src/test/java/com/line4thon/fin4u/Fin4uApplicationTests.java
@@ -2,8 +2,12 @@ package com.line4thon.fin4u;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+// 해당 부분은 DB 없이 스프링 빈들이 정상적으로 기동되는지(컨텍스트 로딩)만 확인
+@SpringBootTest(properties = {
+        "spring.autoconfigure.exclude=org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration"
+})
 class Fin4uApplicationTests {
 
 	@Test
