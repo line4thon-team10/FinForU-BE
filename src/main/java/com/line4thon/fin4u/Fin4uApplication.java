b5a3cf21861f0d37efd8a2476571aac3ff471d74
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-07 20:54:35 +0900

    Feat: SpringBootApplication 객체에 EnableCaching 어노테이션 추가

--- src/main/java/com/line4thon/fin4u/Fin4uApplication.java
+++ src/main/java/com/line4thon/fin4u/Fin4uApplication.java
@@ -2,7 +2,9 @@ package com.line4thon.fin4u;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
+@EnableCaching
 @SpringBootApplication
 public class Fin4uApplication {
 
