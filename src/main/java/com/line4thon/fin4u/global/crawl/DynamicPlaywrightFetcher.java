94e369afcc581eb34f3a1598c8b321e230669dc2
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-08 23:35:21 +0900

    Fix: 은행사 선택할 수 있게 수정

--- src/main/java/com/line4thon/fin4u/global/crawl/DynamicPlaywrightFetcher.java
+++ src/main/java/com/line4thon/fin4u/global/crawl/DynamicPlaywrightFetcher.java
@@ -5,6 +5,7 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.PlaywrightException;
+import com.microsoft.playwright.options.WaitForSelectorState;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -17,10 +18,17 @@ public class DynamicPlaywrightFetcher implements IFetcher {
     private Browser browser;
 
     @Override
-    public String fetch(String url) {
+    public String fetch(String url, String bank) {
         try (BrowserContext context = browser.newContext(new Browser.NewContextOptions().setLocale("ko-KR"))) {
             Page page = context.newPage();
             page.navigate(url, new Page.NavigateOptions().setTimeout(30000));
+
+            page.selectOption("select", bank);
+            page.locator("a").click();
+
+            page.waitForSelector("tbody callback wait", new Page.WaitForSelectorOptions()
+                    .setState(WaitForSelectorState.VISIBLE)
+                    .setTimeout(10000));
 
             return page.content();
         } catch (PlaywrightException e) {
