bd135f2e19c9d229497da264c329b23ead8c3b37
LgE02 <leegaeun023@gmail.com>
2025-11-08 19:20:12 +0900

    chore: 예시 코드 삭제

--- src/main/java/com/line4thon/fin4u/domain/example/service/ExampleService.java
+++
@@ -1,7 +0,0 @@
-package com.line4thon.fin4u.domain.example.service;
-
-import com.line4thon.fin4u.domain.example.web.dto.ExampleResponse;
-
-public interface ExampleService {
-    ExampleResponse getExample(Long id);
-}
