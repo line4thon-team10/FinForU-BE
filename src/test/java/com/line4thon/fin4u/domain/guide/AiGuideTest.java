85bd9ed7420ca72785049f8338dc4564622f9a44
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-08 13:00:11 +0900

    Docs: 사용하지 않는 의존성 import 구문 제거

--- src/test/java/com/line4thon/fin4u/domain/guide/AiGuideTest.java
+++ src/test/java/com/line4thon/fin4u/domain/guide/AiGuideTest.java
@@ -7,13 +7,11 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Answers;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.mapping.JpaMetamodelMappingContext;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.mockito.ArgumentMatchers.anyString;
