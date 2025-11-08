a34b136ae35b3bcf0beb21ff4a153adca1811131
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-08 13:00:38 +0900

    Fix: 한국 은행 API를 호출하는 메소드 더미를 null로 채우기 -> 더미값으로 채우기로 변경

--- src/test/java/com/line4thon/fin4u/domain/exrate/ExRateTest.java
+++ src/test/java/com/line4thon/fin4u/domain/exrate/ExRateTest.java
@@ -2,8 +2,10 @@ package com.line4thon.fin4u.domain.exrate;
 
 import com.line4thon.fin4u.domain.exrate.KoreanBank.Country;
 import com.line4thon.fin4u.domain.exrate.KoreanBank.KoreanBankApiProvider;
+import com.line4thon.fin4u.domain.exrate.entity.ExchangeFee;
 import com.line4thon.fin4u.domain.exrate.service.ExchangeRateServiceImpl;
 import com.line4thon.fin4u.domain.exrate.web.dto.ExchangeRateRes;
+import com.line4thon.fin4u.support.IntegrationTestSupport;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -13,6 +15,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.ai.anthropic.AnthropicChatModel;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -38,21 +41,21 @@ public class ExRateTest {
                 .callExchangeRate(
                         Country.USD_CODE.getCurrencyCode()))
                 .willReturn(new ExchangeRateRes(
-                        new ExchangeRateRes.ExchangeRateData(null,null,null, List.of(new ExchangeRateRes.Exchange(LocalDate.now(), 1.0)) ,null), "")
+                        new ExchangeRateRes.ExchangeRateData(Country.USD_CODE.getCurrencyType(),0.0,0.0, List.of(new ExchangeRateRes.Exchange(LocalDate.now(), 1.0)), getFees(Country.USD_CODE.getCurrencyCode())), "usd")
                         .ExchangeRateData());
 
         given(koreanBankProvider
                 .callExchangeRate(
                         Country.CNY_CODE.getCurrencyCode()))
                 .willReturn(new ExchangeRateRes(
-                        new ExchangeRateRes.ExchangeRateData(null,null,null, List.of(new ExchangeRateRes.Exchange(LocalDate.now(), 1.0)) ,null), "")
+                        new ExchangeRateRes.ExchangeRateData(Country.CNY_CODE.getCurrencyType(),0.0,0.0, List.of(new ExchangeRateRes.Exchange(LocalDate.now(), 1.0)) ,getFees(Country.CNY_CODE.getCurrencyCode())), "cny")
                         .ExchangeRateData());
 
         given(koreanBankProvider
                 .callExchangeRate(
                         Country.VND_CODE.getCurrencyCode()))
                 .willReturn(new ExchangeRateRes(
-                        new ExchangeRateRes.ExchangeRateData(null,null,null, List.of(new ExchangeRateRes.Exchange(LocalDate.now(), 1.0)) ,null), "")
+                        new ExchangeRateRes.ExchangeRateData(Country.VND_CODE.getCurrencyType(),0.0,0.0, List.of(new ExchangeRateRes.Exchange(LocalDate.now(), 1.0)) ,getFees(Country.VND_CODE.getCurrencyCode())), "vnd")
                         .ExchangeRateData());
 
         List<ExchangeRateRes> response = service.getExchangeRateGraphData();
@@ -66,5 +69,38 @@ public class ExRateTest {
         verify(koreanBankProvider).callExchangeRate(Country.USD_CODE.getCurrencyCode());
         verify(koreanBankProvider).callExchangeRate(Country.CNY_CODE.getCurrencyCode());
         verify(koreanBankProvider).callExchangeRate(Country.VND_CODE.getCurrencyCode());
+    }
+
+    private List<ExchangeRateRes.Fee> getFees(String currencyCode) {
+        List<ExchangeRateRes.Fee> eachBankFee = new ArrayList<>();
+        switch(Country.getCurrencyType(currencyCode)) {
+            case "USD":
+                for(ExchangeFee fee : ExchangeFee.values()) {
+                    eachBankFee.add(new ExchangeRateRes.Fee(
+                            fee.getBank(),
+                            fee.getUsdFee()
+                    ));
+                }
+                break;
+            case "CNY":
+                for(ExchangeFee fee : ExchangeFee.values()) {
+                    eachBankFee.add(new ExchangeRateRes.Fee(
+                            fee.getBank(),
+                            fee.getCnyFee()
+                    ));
+                }
+                break;
+            case "VND":
+                for(ExchangeFee fee : ExchangeFee.values()) {
+                    eachBankFee.add(new ExchangeRateRes.Fee(
+                            fee.getBank(),
+                            fee.getVndFee()
+                    ));
+                }
+                break;
+            default:
+                throw new IllegalStateException("Unknown currency code " + currencyCode);
+        }
+        return eachBankFee;
     }
 }
