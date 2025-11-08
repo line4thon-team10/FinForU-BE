a423a79589b6e2edc000fcd0f44c52465d177364
LgE02 <leegaeun023@gmail.com>
2025-11-09 08:20:37 +0900

    chore: @Repository 어노테이션 추가

--- src/main/java/com/line4thon/fin4u/domain/product/repository/custom/SavingCustomRepository.java
+++ src/main/java/com/line4thon/fin4u/domain/product/repository/custom/SavingCustomRepository.java
@@ -3,9 +3,10 @@ package com.line4thon.fin4u.domain.product.repository.custom;
 import com.line4thon.fin4u.domain.product.entity.Deposit;
 import com.line4thon.fin4u.domain.product.entity.InstallmentSaving;
 import com.line4thon.fin4u.domain.product.web.dto.ProductFilterReq;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
-
+@Repository
 public interface SavingCustomRepository {
     List<InstallmentSaving> searchProducts(ProductFilterReq filter);
     List<InstallmentSaving> searchProducts(ProductFilterReq filter, List<Long> ids);
