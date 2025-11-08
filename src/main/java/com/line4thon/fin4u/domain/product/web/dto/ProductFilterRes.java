b2bc417c786af4236a816e303ccade4457e02ce9
LgE02 <leegaeun023@gmail.com>
2025-11-09 04:14:23 +0900

    chore: card 상품 소개글 반환하도록 수정

--- src/main/java/com/line4thon/fin4u/domain/product/web/dto/ProductFilterRes.java
+++ src/main/java/com/line4thon/fin4u/domain/product/web/dto/ProductFilterRes.java
@@ -1,8 +1,10 @@
 package com.line4thon.fin4u.domain.product.web.dto;
 
 import com.line4thon.fin4u.domain.product.entity.Card;
+import com.line4thon.fin4u.domain.product.entity.CardBenefit;
 import com.line4thon.fin4u.domain.product.entity.Deposit;
 import com.line4thon.fin4u.domain.product.entity.InstallmentSaving;
+import com.line4thon.fin4u.domain.product.entity.enums.BenefitCategory;
 
 import java.util.List;
 
@@ -15,14 +17,32 @@ public record ProductFilterRes (
             Long id,
             String name,
             String bankName,
-            int annualFee
+            String feeAndBenefit //프로모션 + 연회비
     ) {
         public static CardProductRes fromCard(Card card) {
+
+            String promotion = card.getCardBenefit().stream()
+                    .filter(b -> b.getBenefitCategory() == BenefitCategory.PROMOTION)
+                    .map(CardBenefit::getDescription)
+                    .findFirst()
+                    .orElse(null);
+
+            String fee;
+            if(card.getDomesticAnnualFee() == 0)
+                fee = "no annual fee";
+            else fee = card.getDomesticAnnualFee()+"won fee.";
+
+            String finalOutput;
+            if(promotion != null && !promotion.isBlank()){
+                finalOutput = promotion + " and " + fee;
+            }else finalOutput = fee;
+
             return new CardProductRes(
                     card.getId(),
                     card.getName(),
                     card.getBank().getBankName(),
-                    card.getAnnualFee()
+                    finalOutput
+
             );
         }
     }
