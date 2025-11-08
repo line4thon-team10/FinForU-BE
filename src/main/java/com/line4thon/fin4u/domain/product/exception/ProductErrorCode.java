0c8dbe2cad79e2ed4bb2d17dfb57ad8f72dcbcda
LgE02 <leegaeun023@gmail.com>
2025-11-09 03:44:46 +0900

    chore: 상품 관련 404에러 처리

--- src/main/java/com/line4thon/fin4u/domain/product/exception/ProductErrorCode.java
+++ src/main/java/com/line4thon/fin4u/domain/product/exception/ProductErrorCode.java
@@ -7,7 +7,10 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public enum ProductErrorCode implements BaseResponseCode {
-    ;
+    NOT_FOUND_CARD_404("NOT_FOUND_CARD_404", 404, "해당 카드 상품을 찾을 수 없습니다."),
+    NOT_FOUND_DEPOSIT_404("NOT_FOUND_DEPOSIT_404", 404, "해당 예금 상품을 찾을 수 없습니다."),
+    NOT_FOUND_SAVING_404("NOT_FOUND_SAVING_404", 404, "해당 적금 상품을 찾을 수 없습니다."),
+    INVALID_PRODUCT_TYPE_400("INVALID_PRODUCT_TYPE_400", 400,"유효하지 않은 상품 타입입니다." );
 
     private final String code;
     private final int httpStatus;
