554bfbd04d838139b403bc050d0ad3802b2bfaf2
LgE02 <leegaeun023@gmail.com>
2025-11-09 07:38:47 +0900

    chore: repository 어노테이션 추가

--- src/main/java/com/line4thon/fin4u/domain/member/repository/MemberRepository.java
+++ src/main/java/com/line4thon/fin4u/domain/member/repository/MemberRepository.java
@@ -2,10 +2,11 @@ package com.line4thon.fin4u.domain.member.repository;
 
 import com.line4thon.fin4u.domain.member.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.math.BigInteger;
 import java.util.Optional;
-
+@Repository
 public interface MemberRepository extends JpaRepository<Member, Long> {
     boolean existsByEmail(String email);
     Optional<Member> findByEmail(String email);
