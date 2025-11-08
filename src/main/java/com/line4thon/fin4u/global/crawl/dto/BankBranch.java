23f156964be194b5b9c04a710104086602863564
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-08 23:59:04 +0900

    Fix: 주석 추가

--- src/main/java/com/line4thon/fin4u/global/crawl/dto/BankBranch.java
+++ src/main/java/com/line4thon/fin4u/global/crawl/dto/BankBranch.java
@@ -1,12 +1,12 @@
 package com.line4thon.fin4u.global.crawl.dto;
 
 public record BankBranch(
-        String type,
-        String bankName,
-        String branchName,
-        String address,
-        String operatingDay,
-        String operatingTime,
-        String info
+        String type,            // 점포 유형
+        String bankName,        // 은행사
+        String branchName,      // 점포 이름
+        String address,         // 주소
+        String operatingDay,    // 운영일
+        String operatingTime,   // 운영시간
+        String info             // 안내
 ) {
 }
