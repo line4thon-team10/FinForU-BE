a1f0ae1032709cf3e05de31197caf1e2afe38bc0
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-10 19:23:33 +0900

    Feat: 은행 이름만으로 List<> 조회할 수 있는 쿼리 메소드 선언

--- src/main/java/com/line4thon/fin4u/global/crawl/repository/ForeignerStoreRepository.java
+++ src/main/java/com/line4thon/fin4u/global/crawl/repository/ForeignerStoreRepository.java
@@ -4,6 +4,9 @@ import com.line4thon.fin4u.global.crawl.Entity.ForeignerStore;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ForeignerStoreRepository extends JpaRepository<ForeignerStore, Long> {
+    List<ForeignerStore> findByBankName(String bankName);
 }
