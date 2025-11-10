ea0760e16e23238f81b02853e7bb054436bcac26
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-10 23:52:01 +0900

    Fix: 지갑 내 카드객체의 수정자 메소드와 누락되었던 Bank 필드 추가

--- src/main/java/com/line4thon/fin4u/domain/wallet/entity/WalletCard.java
+++ src/main/java/com/line4thon/fin4u/domain/wallet/entity/WalletCard.java
@@ -1,13 +1,16 @@
 package com.line4thon.fin4u.domain.wallet.entity;
 
+import com.line4thon.fin4u.domain.wallet.entity.enumulate.Bank;
 import com.line4thon.fin4u.domain.wallet.entity.enumulate.CardType;
 import com.line4thon.fin4u.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Builder
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -16,6 +19,9 @@ public class WalletCard extends BaseEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "card_id")
     private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private Bank bank;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "wallet_id")
@@ -28,4 +34,12 @@ public class WalletCard extends BaseEntity {
     @Column(nullable = false)
     private String cardName;
     private Integer paymentDate;
+
+    @Builder(builderMethodName = "modify")
+    public WalletCard(CardType cardType, Bank bank, String cardName, Integer paymentDate) {
+        this.cardType = cardType;
+        this.bank = bank;
+        this.cardName = cardName;
+        this.paymentDate = paymentDate;
+    }
 }
