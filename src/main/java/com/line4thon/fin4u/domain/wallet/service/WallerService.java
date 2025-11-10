b3aabef78f5fc150f23570924bed48d11bf48626
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-10 20:52:41 +0900

    Feat: 서비스 메소드 작성 및 wallet 레포지토리 변수를 서비스 구현체에 추가

--- src/main/java/com/line4thon/fin4u/domain/wallet/service/WallerService.java
+++ src/main/java/com/line4thon/fin4u/domain/wallet/service/WallerService.java
@@ -1,4 +1,25 @@
 package com.line4thon.fin4u.domain.wallet.service;
 
+import com.line4thon.fin4u.domain.wallet.entity.CheckingAccount;
+import com.line4thon.fin4u.domain.wallet.entity.SavingAccount;
+
 public interface WallerService {
+
+    // Main Page
+    Object getWalletMainPage(Long memberId);
+
+    // ADD
+    Object addCheckingAccount(Long memberId);
+    Object addSavingAccount(Long memberId);
+    Object addCard(Long memberId);
+
+    // EDIT
+    Object editCheckAccountDetail(Long memberId);
+    Object editCardDetail(Long memberId);
+    Object editSavingAccountDetail(Long memberId);
+
+    // DELETE
+    Void deleteCheckAccount(Long memberId);
+    Void deleteSavingAccount(Long memberId);
+    Void deleteCard(Long memberId);
 }
--- src/main/java/com/line4thon/fin4u/domain/wallet/service/WalletServiceImpl.java
+++ src/main/java/com/line4thon/fin4u/domain/wallet/service/WalletServiceImpl.java
@@ -4,9 +4,11 @@ import com.line4thon.fin4u.domain.member.repository.MemberRepository;
 import com.line4thon.fin4u.domain.wallet.repository.CardRepository;
 import com.line4thon.fin4u.domain.wallet.repository.CheckingAccountRepository;
 import com.line4thon.fin4u.domain.wallet.repository.SavingAccountRepository;
+import com.line4thon.fin4u.domain.wallet.repository.WallerRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
@@ -14,7 +16,67 @@ import org.springframework.stereotype.Service;
 public class WalletServiceImpl implements WallerService{
 
     private final MemberRepository memberRepository;
+    private final WallerRepository walletRepository;
     private final SavingAccountRepository savingAccountRepository;
     private final CheckingAccountRepository checkingAccountRepository;
     private final CardRepository cardRepository;
+
+    @Override
+    public Object getWalletMainPage(Long memberId) {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public Object addCheckingAccount(Long memberId) {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public Object addSavingAccount(Long memberId) {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public Object addCard(Long memberId) {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public Object editCheckAccountDetail(Long memberId) {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public Object editCardDetail(Long memberId) {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public Object editSavingAccountDetail(Long memberId) {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public Void deleteCheckAccount(Long memberId) {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public Void deleteSavingAccount(Long memberId) {
+        return null;
+    }
+
+    @Override
+    @Transactional
+    public Void deleteCard(Long memberId) {
+        return null;
+    }
 }
