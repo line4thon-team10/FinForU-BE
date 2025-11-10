a586dc677e6f67aa5b641f502fc5272a237afd09
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-10 23:55:28 +0900

    Feat: 입출금, 보통 예금 계좌의 수정, 추가 요청 발생 시 json -> dto 변환할 객체 작성

---
+++ src/main/java/com/line4thon/fin4u/domain/wallet/web/dto/CheckingAccountReq.java
@@ -0,0 +1,9 @@
+package com.line4thon.fin4u.domain.wallet.web.dto;
+
+import com.line4thon.fin4u.domain.wallet.entity.enumulate.Bank;
+import jakarta.validation.constraints.NotNull;
+
+public class CheckingAccountReq {
+    @NotNull(message = "은행은 비어있을 수 없습니다.")
+    private Bank bank;
+}
