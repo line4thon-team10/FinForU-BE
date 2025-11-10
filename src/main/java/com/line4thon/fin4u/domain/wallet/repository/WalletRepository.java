1e872e89264404405ca35aff87430354525ed225
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-10 20:59:08 +0900

    Feat: member Id로 wallet을 조회하는 Optional 메소드 작성

--- src/main/java/com/line4thon/fin4u/domain/wallet/repository/WalletRepository.java
+++ src/main/java/com/line4thon/fin4u/domain/wallet/repository/WalletRepository.java
@@ -3,5 +3,8 @@ package com.line4thon.fin4u.domain.wallet.repository;
 import com.line4thon.fin4u.domain.wallet.entity.Wallet;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
+    Optional<Wallet> findByMemberId(Long memberId);
 }
