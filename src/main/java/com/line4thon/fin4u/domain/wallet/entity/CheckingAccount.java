a440186fefbab6c55a336d150a1c099fe09d00a5
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-10 20:22:56 +0900

    Feat: 입출금, 보통 예금 계좌 엔터티 작성

---
+++ src/main/java/com/line4thon/fin4u/domain/wallet/entity/CheckingAccount.java
@@ -0,0 +1,21 @@
+package com.line4thon.fin4u.domain.wallet.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CheckingAccount {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "checking_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "wallet_id", nullable = false)
+    private Wallet wallet;
+}
