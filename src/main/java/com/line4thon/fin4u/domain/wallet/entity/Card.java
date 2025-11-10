3a6df2e54f7ba6024ff2caa9c31d6be7ad7d4b09
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-10 20:29:15 +0900

    Feat: 통합 카드 엔터티 객체 작성

---
+++ src/main/java/com/line4thon/fin4u/domain/wallet/entity/Card.java
@@ -0,0 +1,29 @@
+package com.line4thon.fin4u.domain.wallet.entity;
+
+import com.line4thon.fin4u.global.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Card extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "card_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "wallet_id")
+    private Wallet wallet;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private String cardType;
+
+    private String cardName;
+    private Integer paymentDate;
+}
