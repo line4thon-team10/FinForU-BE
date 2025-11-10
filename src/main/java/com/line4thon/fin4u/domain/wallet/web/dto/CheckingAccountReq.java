f0c7b309bcad2e90a3b4620d4ccd539fb9ec76a2
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-11 00:02:50 +0900

    Fix: 지정할 수 있는 상품 별명에서 은행 이름으로 변경

--- src/main/java/com/line4thon/fin4u/domain/wallet/web/dto/CheckingAccountReq.java
+++ src/main/java/com/line4thon/fin4u/domain/wallet/web/dto/CheckingAccountReq.java
@@ -2,7 +2,11 @@ package com.line4thon.fin4u.domain.wallet.web.dto;
 
 import com.line4thon.fin4u.domain.wallet.entity.enumulate.Bank;
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
 
+@Getter
+@AllArgsConstructor
 public class CheckingAccountReq {
     @NotNull(message = "은행은 비어있을 수 없습니다.")
     private Bank bank;
