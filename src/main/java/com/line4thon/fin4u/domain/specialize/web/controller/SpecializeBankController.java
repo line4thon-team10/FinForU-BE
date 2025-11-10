890cb3cacc36612ac77b614ea32adc14c348e627
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-10 19:58:13 +0900

    Feat: path variable로 은행사 이름을 받으면 그 값으로 조회하여 반환하는 get 컨트롤러 메소드 작성

--- src/main/java/com/line4thon/fin4u/domain/specialize/web/controller/SpecializeBankController.java
+++ src/main/java/com/line4thon/fin4u/domain/specialize/web/controller/SpecializeBankController.java
@@ -5,11 +5,14 @@ import com.line4thon.fin4u.domain.specialize.web.dto.SpecializeBankRes;
 import com.line4thon.fin4u.global.crawl.Entity.ForeignerStore;
 import com.line4thon.fin4u.global.crawl.repository.ForeignerStoreRepository;
 import com.line4thon.fin4u.global.response.SuccessResponse;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,9 +29,19 @@ public class SpecializeBankController {
     public ResponseEntity<SuccessResponse<?>> getSpecializeBankInfo() {
 
         List<ForeignerStore> result = repository.findAll();
-        if (result.isEmpty()) {
-            throw new SpecializeBankNotFoundException();
-        }
+        return buildAndReturnResponseEntity(result);
+    }
+
+    @GetMapping("/{bank}")
+    public ResponseEntity<SuccessResponse<?>> getSpecializeBankInfo(@PathVariable @NotBlank(message = "bank pathVariable은 비어있을 수 없습니다.") String bank) {
+        List<ForeignerStore> result = repository.findByBankName(bank);
+
+        return buildAndReturnResponseEntity(result);
+    }
+
+    @NotNull
+    private ResponseEntity<SuccessResponse<?>> buildAndReturnResponseEntity(List<ForeignerStore> result) {
+        if(result.isEmpty()) throw new SpecializeBankNotFoundException();
 
         List<SpecializeBankRes> response = result.stream()
                 .map(record -> new SpecializeBankRes(
