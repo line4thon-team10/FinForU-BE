a888b060c4181ca1e9613b127e73cd8dbeb2ff62
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-10 23:52:58 +0900

    Fix: 지갑 내 member 객체의 MemberId로 조회하기 때문에, findByMemberId -> findByMemberMemberId로 메소드명 수정

--- src/main/java/com/line4thon/fin4u/domain/wallet/repository/WalletRepository.java
+++ src/main/java/com/line4thon/fin4u/domain/wallet/repository/WalletRepository.java
@@ -8,5 +8,5 @@ import java.util.Optional;
 
 @Repository
 public interface WalletRepository extends JpaRepository<Wallet, Long> {
-    Optional<Wallet> findByMemberId(Long memberId);
+    Optional<Wallet> findByMemberMemberId(Long memberId);
 }
