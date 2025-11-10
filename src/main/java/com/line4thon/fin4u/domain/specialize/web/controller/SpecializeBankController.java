37a8ef3f24e84d0381cf97d5919707cc0e3f307d
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-10 18:47:56 +0900

    Feat: 외국인 특화점포 정보를 조회하는 GET 메소드, 컨트롤러 작성

---
+++ src/main/java/com/line4thon/fin4u/domain/specialize/web/controller/SpecializeBankController.java
@@ -0,0 +1,34 @@
+package com.line4thon.fin4u.domain.specialize.web.controller;
+
+import com.line4thon.fin4u.global.crawl.Entity.ForeignerStore;
+import com.line4thon.fin4u.global.crawl.repository.ForeignerStoreRepository;
+import com.line4thon.fin4u.global.response.SuccessResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@Slf4j
+@RestController
+@RequestMapping("/specialize")
+@RequiredArgsConstructor
+public class SpecializeBankController {
+    private final ForeignerStoreRepository repository;
+
+    @GetMapping
+    public ResponseEntity<SuccessResponse<?>> getSpecializeBankInfo() {
+
+        List<ForeignerStore> response = repository.findAll();
+
+        if (response.isEmpty()) {
+            throw new RuntimeException();
+        }
+
+        return ResponseEntity.status(HttpStatus.OK).body(SuccessResponse.ok(response));
+    }
+}
