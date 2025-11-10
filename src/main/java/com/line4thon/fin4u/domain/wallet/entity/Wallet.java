d85dcb5ffbbe026770ae09de53898a1ccc981479
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-10 23:51:24 +0900

    Fix: 지갑 객체 내에 지갑 메인화면을 조회할 때 필요한 다른 테이블들의 조인 명시

--- src/main/java/com/line4thon/fin4u/domain/wallet/entity/Wallet.java
+++ src/main/java/com/line4thon/fin4u/domain/wallet/entity/Wallet.java
@@ -4,6 +4,7 @@ import com.line4thon.fin4u.domain.member.entity.Member;
 import com.line4thon.fin4u.global.entity.BaseEntity;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
@@ -13,6 +14,7 @@ import java.util.List;
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Wallet extends BaseEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
