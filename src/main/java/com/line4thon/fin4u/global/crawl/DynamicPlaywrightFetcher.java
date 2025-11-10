442aa8cd521f78e39c406b485478015ecbaccf91
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-10 18:39:08 +0900

    Feat: 동적 사이트 크롤링 객체 작성

--- src/main/java/com/line4thon/fin4u/global/crawl/DynamicPlaywrightFetcher.java
+++ src/main/java/com/line4thon/fin4u/global/crawl/DynamicPlaywrightFetcher.java
@@ -5,6 +5,8 @@ import com.microsoft.playwright.Browser;
 import com.microsoft.playwright.BrowserContext;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.PlaywrightException;
+import com.microsoft.playwright.options.AriaRole;
+import com.microsoft.playwright.options.SelectOption;
 import com.microsoft.playwright.options.WaitForSelectorState;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,13 +25,34 @@ public class DynamicPlaywrightFetcher implements IFetcher {
             Page page = context.newPage();
             page.navigate(url, new Page.NavigateOptions().setTimeout(30000));
 
-            page.selectOption("select", bank);
+//            String selectSelector = "select";
+//            log.debug("[Fetcher] '{}' 드롭다운에서 '{}' 레이블 선택 시도...",selectSelector, bank);
+//            page.selectOption(selectSelector, new SelectOption().setLabel(bank));
+//            log.debug("[Fetcher] 은행 선택 완료: {}", bank);
+//
+//            log.debug("   [Fetcher] '검색' 버튼 클릭 시도...");
+//            page.locator("a.btnSearch01").click();
+//            log.debug("   [Fetcher] '검색' 버튼 클릭 완료.");
+//
+            page.selectOption("select#BankCode", bank);
+            page.waitForNavigation(() -> {
+                page.getByRole(AriaRole.LINK, new Page.GetByRoleOptions().setName("검색").setExact(true)).click();
+            });
+            String tableSelector = "table.resultList_ty02 tbody tr";
+//            log.debug("[Fetcher] 결과 테이블 '{}' 로드 대기...", tableSelector);
+            page.waitForSelector(tableSelector,
+                    new Page.WaitForSelectorOptions()
+                            .setState(WaitForSelectorState.VISIBLE)
+                            .setTimeout(10000));
+            log.debug("[Fetcher] 결과 테이블 로드 완료.");
+
+            /*
             page.locator("a").click();
 
             page.waitForSelector("#Table_Result tbody tr", new Page.WaitForSelectorOptions()
                     .setState(WaitForSelectorState.VISIBLE)
                     .setTimeout(10000));
-
+*/
             return page.content();
         } catch (PlaywrightException e) {
             log.error(e.getMessage(), e);
