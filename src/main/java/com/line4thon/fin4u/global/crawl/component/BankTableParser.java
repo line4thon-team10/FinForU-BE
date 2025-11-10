ea657cd516727e7298c14ea2c1a7687c647f6b76
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-10 18:37:44 +0900

    Fix: 사이트 내 은행 테이블 파싱하는 객체 작성

--- src/main/java/com/line4thon/fin4u/global/crawl/component/BankTableParser.java
+++ src/main/java/com/line4thon/fin4u/global/crawl/component/BankTableParser.java
@@ -2,7 +2,7 @@ package com.line4thon.fin4u.global.crawl.component;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.line4thon.fin4u.global.crawl.dto.BankBranch;
+import com.line4thon.fin4u.global.crawl.web.dto.BankBranch;
 import lombok.extern.slf4j.Slf4j;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -26,7 +26,7 @@ public class BankTableParser implements IParser {
         try {
             Document document = Jsoup.parse(htmlContent);
 
-            Element table = document.selectFirst("#Table_Result");
+            Element table = document.selectFirst("table.resultList_ty02");
 
             if(table == null) {
                 data.setContent("");
@@ -40,15 +40,24 @@ public class BankTableParser implements IParser {
             for (Element row : rows) {
                 Elements cells = row.select("td");
 
-                if(cells.size() >= 7) {
+                if(cells.size() == 6) {
+                    Element viewCell = cells.get(5);
+                    Element viewLink = viewCell.selectFirst("a[href]");
+
+                    String branchId = "";
+
+                    if(viewLink != null) {
+                        String href = viewLink.attr("href");
+                        branchId = href.replaceAll("[^0-9]", "");
+                    }
                     BankBranch branch = new BankBranch(
                             cells.get(0).text(),
                             cells.get(1).text(),
                             cells.get(2).text(),
-                            cells.get(3).text(),
                             cells.get(4).text(),
                             cells.get(5).text(),
-                            cells.get(6).text()
+                            branchId,
+                            ""
                     );
 
                     branches.add(branch);
@@ -65,7 +74,7 @@ public class BankTableParser implements IParser {
 
             data.setNewUrls(newUrls);
         } catch (JsonProcessingException e) {
-            log.error("Json Parse Failed", e);
+            log.error("Json 파싱 실패", e);
             data.setContent("");
             data.setNewUrls(new ArrayList<>());
         }
