22c564a414b2dc8c1bfa343e482adeaf1f126f62
LgE02 <leegaeun023@gmail.com>
2025-11-10 13:28:52 +0900

    chore: 요청부분 string에서 enum으로 수정

--- src/main/java/com/line4thon/fin4u/domain/product/service/Product/ProductService.java
+++ src/main/java/com/line4thon/fin4u/domain/product/service/Product/ProductService.java
@@ -1,5 +1,6 @@
 package com.line4thon.fin4u.domain.product.service.Product;
 
+import com.line4thon.fin4u.domain.product.entity.enums.Type;
 import com.line4thon.fin4u.domain.product.web.dto.ProductDetailRes;
 import com.line4thon.fin4u.domain.product.web.dto.ProductFilterReq;
 import com.line4thon.fin4u.domain.product.web.dto.ProductFilterRes;
@@ -7,5 +8,5 @@ import com.line4thon.fin4u.domain.product.web.dto.ProductFilterRes;
 public interface ProductService {
     ProductFilterRes getFilterProduct(ProductFilterReq filter);
 
-    ProductDetailRes getProductDetail(String type, Long id);
+    ProductDetailRes getProductDetail(Type type, Long id);
 }
