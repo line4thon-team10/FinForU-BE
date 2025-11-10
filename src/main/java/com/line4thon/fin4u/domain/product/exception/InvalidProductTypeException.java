8f32b4bcbd4513b0491445eb77e59f23e79fcf38
LgE02 <leegaeun023@gmail.com>
2025-11-10 19:22:02 +0900

    chore: exception 삭제

--- src/main/java/com/line4thon/fin4u/domain/product/exception/InvalidProductTypeException.java
+++
@@ -1,9 +0,0 @@
-package com.line4thon.fin4u.domain.product.exception;
-
-import com.line4thon.fin4u.global.exception.BaseException;
-
-public class InvalidProductTypeException extends BaseException {
-    public InvalidProductTypeException() {
-        super(ProductErrorCode.INVALID_PRODUCT_TYPE_400);
-    }
-}
