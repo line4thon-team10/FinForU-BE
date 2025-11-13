e2f8c2db210758dad37b20f728c8c2777b4fb842
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-13 12:15:27 +0900

    Fix: CardReq DTO 객체 내 각 필드에 검증의 범위를 지정

--- src/main/java/com/line4thon/fin4u/domain/wallet/web/dto/CardReq.java
+++ src/main/java/com/line4thon/fin4u/domain/wallet/web/dto/CardReq.java
@@ -2,6 +2,8 @@ package com.line4thon.fin4u.domain.wallet.web.dto;
 
 import com.line4thon.fin4u.domain.wallet.entity.enumulate.Bank;
 import com.line4thon.fin4u.domain.wallet.entity.enumulate.CardType;
+import com.line4thon.fin4u.domain.wallet.web.dto.groups.ForSave;
+import com.line4thon.fin4u.domain.wallet.web.dto.groups.ForUpdate;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
@@ -12,15 +14,16 @@ import java.time.LocalDate;
 @Getter
 @AllArgsConstructor
 public class CardReq {
+    @NotNull(groups = ForUpdate.class)
     private Long cardId;
 
-    @NotNull(message = "은행 종류는 비어있을 수 없습니다.")
+    @NotNull(groups = {ForSave.class, ForUpdate.class}, message = "은행 종류는 비어있을 수 없습니다.")
     private Bank bank;
 
-    @NotNull(message = "카드 종류는 비어있을 수 없습니다.")
+    @NotNull(groups = {ForSave.class, ForUpdate.class}, message = "카드 종류는 비어있을 수 없습니다.")
     private CardType cardType;
 
-    @NotBlank(message = "상품 별명은 비어있을 수 없습니다.")
+    @NotBlank(groups = {ForSave.class, ForUpdate.class}, message = "상품 별명은 비어있을 수 없습니다.")
     private String cardName;
 
     private LocalDate upcomingDate;
