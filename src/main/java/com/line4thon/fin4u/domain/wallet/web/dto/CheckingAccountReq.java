3c48f1166fd71d9843e0a1dcf48a9033788d3fde
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-13 12:17:37 +0900

    Fix: 보통 예금, 입출금 계좌 추가 혹은 수정할 때 사용하는 DTO 객체 내 필드에 검증의 범위 지정

--- src/main/java/com/line4thon/fin4u/domain/wallet/web/dto/CheckingAccountReq.java
+++ src/main/java/com/line4thon/fin4u/domain/wallet/web/dto/CheckingAccountReq.java
@@ -1,6 +1,8 @@
 package com.line4thon.fin4u.domain.wallet.web.dto;
 
 import com.line4thon.fin4u.domain.wallet.entity.enumulate.Bank;
+import com.line4thon.fin4u.domain.wallet.web.dto.groups.ForSave;
+import com.line4thon.fin4u.domain.wallet.web.dto.groups.ForUpdate;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -8,8 +10,9 @@ import lombok.Getter;
 @Getter
 @AllArgsConstructor
 public class CheckingAccountReq {
+    @NotNull(groups = ForUpdate.class)
     private Long checkingAccountId;
 
-    @NotNull(message = "은행은 비어있을 수 없습니다.")
+    @NotNull(groups = {ForUpdate.class, ForSave.class}, message = "은행은 비어있을 수 없습니다.")
     private Bank bank;
 }
