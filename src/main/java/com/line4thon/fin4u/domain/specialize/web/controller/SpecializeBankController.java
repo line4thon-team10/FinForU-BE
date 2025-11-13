1608d15f51689532a1e1b14381d5cef2e7bf15f4
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-13 12:05:52 +0900

    Feat: SpecializeBankController 내 PathVariable 인자의 Validation을 위해 컨트롤러 객체에 Validated 어노테이션 추가

--- src/main/java/com/line4thon/fin4u/domain/specialize/web/controller/SpecializeBankController.java
+++ src/main/java/com/line4thon/fin4u/domain/specialize/web/controller/SpecializeBankController.java
@@ -11,6 +11,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @Slf4j
+@Validated
 @RestController
 @RequestMapping("/specialize")
 @RequiredArgsConstructor
