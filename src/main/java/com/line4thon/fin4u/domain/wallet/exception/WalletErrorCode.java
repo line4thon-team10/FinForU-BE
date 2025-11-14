ce6f65449a314fb8e3d48382ab1c3a2b327807bf
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-14 21:53:21 +0900

    Feat: 카드, 예금, 저축 계좌를 찾지 못했을 때 던질 커스텀 예외 코드 작성

--- src/main/java/com/line4thon/fin4u/domain/wallet/exception/WalletErrorCode.java
+++ src/main/java/com/line4thon/fin4u/domain/wallet/exception/WalletErrorCode.java
@@ -9,7 +9,10 @@ import lombok.Getter;
 public enum WalletErrorCode implements BaseResponseCode {
 
     PRINCIPAL_NOT_READABLE("PRINCIPAL_NOT_READABLE", 401, "jwt를 읽을 수 없습니다."),
-    WALLET_NOT_FOUND("WALLET_NOT_FOUND_404", 404, "해당 멤버가 소지하고 있는 지갑이 없습니다.");
+    WALLET_NOT_FOUND("WALLET_NOT_FOUND_404", 404, "해당 멤버가 소지하고 있는 지갑이 없습니다."),
+    CARD_NOT_FOUND("CARD_NOT_FOUND_404", 404, "해당 카드를 찾을 수 없습니다."),
+    CHECKING_ACCOUNT_NOT_FOUND("CHECKING_ACCOUNT_NOT_FOUND_404", 404, "해당 예금 계좌를 찾을 수 없습니다."),
+    SAVING_ACCOUNT_NOT_FOUND("SAVING_ACCOUNT_NOT_FOUND_404", 404, "해당 저축 계좌를 찾을 수 없습니다.");
 
     private final String code;
     private final int httpStatus;
