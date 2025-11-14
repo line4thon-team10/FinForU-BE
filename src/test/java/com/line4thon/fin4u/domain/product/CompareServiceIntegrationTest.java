a6d81440ca182c127ebf83c243dd6f09db4073a0
LgE02 <leegaeun023@gmail.com>
2025-11-14 15:45:05 +0900

    chore : 시간문제로....

--- src/test/java/com/line4thon/fin4u/domain/product/CompareServiceIntegrationTest.java
+++ src/test/java/com/line4thon/fin4u/domain/product/CompareServiceIntegrationTest.java
@@ -23,7 +23,6 @@ import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@AutoConfigureMockMvc(addFilters = false)
 @ActiveProfiles("test")
 @Transactional
 class CompareServiceIntegrationTest extends IntegrationTestSupport {
