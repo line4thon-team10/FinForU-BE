ceebea6ab4038709c84fa305feac21c9765f41d1
Joonseok-Lee <dlwnstjr31@hansung.ac.kr>
2025-11-14 17:01:35 +0900

    Fix: 쿼리 파라미터로 특화점포 필터링하는 경로가 securityConfig에 없어서 추가

--- src/main/java/com/line4thon/fin4u/global/security/SecurityConfig.java
+++ src/main/java/com/line4thon/fin4u/global/security/SecurityConfig.java
@@ -36,6 +36,7 @@ public class SecurityConfig {
                                 "/products/**",
                                 "/exrate",
                                 "/specialize",
+                                "/specialize/**",
                                 "/push/**",
                                 "/health"
                         ).permitAll()
